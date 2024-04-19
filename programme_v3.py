0e04071a4551a079f176897b27ce5faaccb70d7f
Stéphane Téletchéa <547146+TeletcheaLab@users.noreply.github.com>
2024-04-19 10:59:01 +0200

    Create programme_v3.py
    Ajout programme v3

---
+++ programme_v3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def affiche_coucou():
+  """
+  Fonction qui permet d'afficher le texte Coucou
+  """
+  print("Coucou")
+
+def main():
+  affiche_coucou()
+
+if __name__ == "__main__":
+  main()
