78bf9b85d933e53c7fe7b3b05cf980e8fd424cbf
Stéphane Téletchéa <547146+TeletcheaLab@users.noreply.github.com>
2024-04-19 11:26:44 +0200

    Create programme_v4.py
    Programme incomplet mais avec du code factoriés

---
+++ programme_v4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def affiche_texte(letexte):
+  """
+  Fonction qui permet d'afficher le texte fourni en argument
+  """
+  print(letexte)
+
+def demande_texte()
+  """
+  Fonction qui demande un mot à afficher à l'utilisateur, pour l'instant elle ne fait rien
+  """
+  pass
+
+def main():
+  demande_texte()
+  affiche_texte("Coucou")
+  affiche_texte("Bonjour")
+
+if __name__ == "__main__":
+  main()
